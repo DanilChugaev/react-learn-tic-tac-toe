7b9c055f43083e7ff5c72d475b728749e5cf50cd
Danil Chugaev <dmchugaev@gmail.com>
2026-01-21 22:53:25 +0700

    adds PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/react-learn-tic-tac-toe/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Tic Tac Toe',
+        short_name: 'Tic Tac Toe',
+        description: 'Tic Tac Toe game',
+        theme_color: '#242424',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg}'],
+      },
+    }),
+  ],
 });
